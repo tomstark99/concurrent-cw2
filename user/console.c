2dbf298a3d5b8e350840abbf6e040e098089084d
tomstark99 <tom.stark.2018@bristol.ac.uk>
2020-03-16 12:23:04 +0000

    terminate try again3

--- user/console.c
+++ user/console.c
@@ -118,7 +118,7 @@ void main_console() {
       }
     } 
     else if( 0 == strcmp( cmd_argv[ 0 ], "terminate" ) ) {
-      printf("%d\n", atoi(cmd_argv[1]));
+      //printf("%d\n", atoi(cmd_argv[1]));
       kill( atoi( cmd_argv[ 1 ] ), SIG_TERM );
     } 
     else {
